27a084500fb1c9f7e99495035f8127f54d35cc56
Tetyana <tsergata@gmail.com>
2020-01-09 17:13:48 +0100

    prove

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-// alert('ciao');
+// Fizz Buzz test:
+// Scrivi un programma che stampi i numeri da 1
+// a 100, ma per i multipli di 3 stampi “Fizz” al
+// posto del numero e per i multipli di 5 stampi
+// Buzz. Per i numeri che sono sia multipli di 3
+// che di 5 stampi FizzBuzz.
+
+// Stampare  i numeri da 1 a 100
+var risultato;
+
+for (var i = 1; i <= 100; i++){
+  console.log(i);
+
+  if(i % 3 === 0 ){
+    risultato = 'Fizz';
+    console.log(risultato);
+  }else if (i % 5 === 0 ) {
+
+    console.log('Buzz');
+  }
+  else if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+  }
+  else {
+    // console.log(i);
+  }
+}
